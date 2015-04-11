3fe7d826cbeb6d0b7a566644c602fd01f96982a4
greg <insanetheta@gmail.com>
2015-04-11 12:24:55 -0700

    re-adding client app

---
+++ MTGDeals/Assets/Scripts/LifeCounter/Calculator/Clear.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Clear : MonoBehaviour 
+{
+	private NumberToCalculate RefToCalc;
+	
+	public void Start()
+	{
+		RefToCalc = this.transform.parent.parent.GetComponentInParent<NumberToCalculate>();
+	}
+	
+	void OnClick()
+	{
+		RefToCalc.ClearDigit();
+		Debug.Log(RefToCalc.TheNum);
+	}
+}
